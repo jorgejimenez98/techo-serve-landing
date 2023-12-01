c3a21a05ec5a8d034653b24c26a1e409e4641be3
Jorge Jimenez <georgeguitarra07@gmail.com>
2023-12-01 21:54:45 +0100

    feat(app): add first version of the header and add some pictures

--- src/assets/images/index.ts
+++ src/assets/images/index.ts
@@ -1,1 +1,2 @@
 export { default as LOGO } from './logo.webp'
+export { default as SECTION_BACKGROUND } from './section-background.webp'
--- src/components/layouts/header/Header.tsx
+++ src/components/layouts/header/Header.tsx
@@ -1,13 +1,51 @@
 import React from 'react'
 
-import { LOGO } from '@/assets/images'
+import { LOGO, SECTION_BACKGROUND } from '@/assets/images'
 import Image from 'next/image'
+import Link from 'next/link'
 
 const Header: React.FC = () => {
+
+  const sectionBackgroundStyle: React.CSSProperties = {
+    background: `url(${SECTION_BACKGROUND})`,
+  }
   return (
     <div>
-      <Image src={LOGO} alt='logo' />
-      Header
+      <nav className='navbar main-container' style={sectionBackgroundStyle}>
+        <div className='navbar-start'>
+          <div className='dropdown'>
+            <label tabIndex={0} className='btn btn-ghost lg:hidden'>
+              <Image
+                src={LOGO}
+                alt='Menu Icon'
+              />
+            </label>
+            <ul
+              tabIndex={0}
+              className='menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52'
+            >
+              <li>Contact</li>
+            </ul>
+          </div>
+          <Link href={'/'}>
+            <Image
+              src={LOGO}
+              alt='Menu Icon'
+            />
+          </Link>
+        </div>
+
+        <div className='navbar-end'>
+          <div className='hidden lg:flex'>
+            <ul className='menu menu-horizontal px-1'>
+              <li>Contact</li>
+            </ul>
+          </div>
+
+          <li>Contact</li>
+        </div>
+      </nav>
+
     </div>
   )
 }
